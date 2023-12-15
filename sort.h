a44ad0dd93bdd1c009d643e8b328707d4410b098
amineNouabi <aminenouabi9@gmail.com>
2023-12-15 23:46:33 +0100

    Counting Sort: v1

--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void swap_int(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 /* Utility Functions for Shell Sort */
 size_t initial_knuth_gap(size_t size);
@@ -34,5 +35,12 @@ size_t initial_knuth_gap(size_t size);
 /* Utility Functions for Cocktail Shaker Sort */
 void swap_right(listint_t *cursor, listint_t **head);
 void swap_left(listint_t *cursor, listint_t **head);
+
+/* Utility Functions for Counting Sort */
+int array_max(int *array, size_t size);
+int *create_counting_array(int *array, size_t size);
+int *create_sorted_array(int *array, size_t size, int *counting_array);
+
+
 
 #endif /* SORT_H */
