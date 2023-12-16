2e95a06e8e211f2cb9ec234ad7084b3a262155cc
radouane.abounouas <radouaneabn@gmail.com>
2023-12-16 17:40:03 +0100

    six declaration before check for NULL

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,15 +8,16 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list, *tmp;
+	listint_t *current, *tmp;
 	int flag;
 
-	if (list == NULL || current == NULL || current->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	current = *list;
 	while (current->next)
 	{
+
 		tmp = current->next;
 		flag = 1;
 
