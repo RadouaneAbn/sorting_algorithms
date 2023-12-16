d5247717482d8447a54090e5161e26b886d7556f
radouane.abounouas <radouaneabn@gmail.com>
2023-12-16 19:47:05 +0100

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * swap_int - This function swaps two integers
+ * @a: the first integer
+ * @b: the second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * selection_sort - This function sorts an array using selection sorting
  * @array: The array to be sorted
  * @size: The size of the array
@@ -28,19 +41,8 @@ void selection_sort(int *array, size_t size)
 		if (flag == 0)
 			break;
 		swap_int(&array[idx], &array[i]);
-		print_array(array, size);
+
+		if (i != idx)
+			print_array(array, size);
 	}
-}
-
-/**
- * swap_int - This function swaps two integers
- * @a: the first integer
- * @b: the second integer
- */
-
-void swap_int(int *a, int *b)
-{
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
 }
