85d48e6698f422159da344c631c89e2bc725d752
radouane.abounouas <radouaneabn@gmail.com>
2023-12-16 17:29:42 +0100

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,28 +2,6 @@
 #include "sort.h"
 
 /**
- * swap - This function swaps two nodes
- * @list: the doubly linked list
- * @a: the first node
- * @b: the second node
- */
-
-void swap(listint_t **list, listint_t *a, listint_t *b)
-{
-	a->next = b->next;
-	b->prev = a->prev;
-	if (a->prev)
-		a->prev->next = b;
-	else
-		*list = b;
-
-	a->prev = b;
-	if (b->next)
-		b->next->prev = a;
-	b->next = a;
-}
-
-/**
  * insertion_sort_list - This function sorts a doubly linked list
  * @list: the doubly linked list
  */
@@ -46,6 +24,7 @@ void insertion_sort_list(listint_t **list)
 		while (tmp->prev && tmp->n < tmp->prev->n)
 		{
 			swap(list, tmp->prev, tmp);
+			print_list(*list);
 			flag = 0 /* the list is not sorted */;
 		}
 
@@ -54,3 +33,24 @@ void insertion_sort_list(listint_t **list)
 	}
 }
 
+/**
+ * swap - This function swaps two nodes
+ * @list: the doubly linked list
+ * @a: the first node
+ * @b: the second node
+ */
+
+void swap(listint_t **list, listint_t *a, listint_t *b)
+{
+	a->next = b->next;
+	b->prev = a->prev;
+	if (a->prev)
+		a->prev->next = b;
+	else
+		*list = b;
+
+	a->prev = b;
+	if (b->next)
+		b->next->prev = a;
+	b->next = a;
+}
