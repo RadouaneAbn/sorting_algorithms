9de05916a13f72fd10384db91de2b9fa6bdd246e
radouane.abounouas <radouaneabn@gmail.com>
2023-12-17 21:41:34 +0100

    update: heap sort

--- sort.h
+++ sort.h
@@ -59,8 +59,6 @@ void quick_sort(int *array, size_t size);
 void sort_quick_Lomuto(int *arr, size_t left, size_t right, size_t size);
 
 /* Utility Functions for heap Sort */
-void builtmaxheap(int *array, size_t n);
 void heap_sort(int *array, size_t size);
-void siftdown(int *array, size_t root, size_t end);
 
 #endif /* SORT_H */
